1f8bd1d025fa4c70740714d965de62f3787e8df7
bobs3400 <124318533+bobs3400@users.noreply.github.com>
2023-02-15 12:24:52 +1000

    домашка 13

---
+++ DomZadanie13/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+if(a < 100)
+{
+    Console.WriteLine("третьей цифры нет");
+} 
+else 
+{
+    int i = 0; 
+    while (a > 999)
+    {
+        i++;
+        a = a /10;
+    }
+    int tri = a % 10;
+    Console.WriteLine("Третья цифра " + tri);
+}
